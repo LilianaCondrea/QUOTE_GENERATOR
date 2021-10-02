3d3838f81cebcd6f91e88fe2794b98eaad450763
LilianaCondrea <l.cndrea@yahoo.com>
2021-10-02 22:55:11 +0300

    quote project

---
+++ quotes.js
@@ -0,0 +1,19 @@
+const localQuotes = [
+    {
+      text: 'Invata web development daca vrei bani multi!',
+      author: null,
+    },
+    {
+      text: 'Web development-ul este de viitor!',
+      author: null,
+    },
+    {
+      text: 'Poti lucra de oriunde!!',
+      author: 'L.D.',
+    },
+    {
+      text: 'Treci la treaba ca timpul trece!',
+      author: 'L.D.',
+    }
+  ];
+  
--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+const quoteContainer=document.getElementById('quote-container');
+const quoteText=document.getElementById('quote');
+const authorText=document.getElementById('author');
+const newQuoteBtn=document.getElementById('new-quote');
+const loader=document.getElementById('loader');
 
+// Show Loading
+function loading(){
+    loader.hidden=false;
+    quoteContainer.hidden=true;
+}
+
+// Hide loading
+function complete(){
+    loader.hidden=true;
+    quoteContainer.hidden=false;
+}
+
+function newQuote(){
+    loading();
+    // pick a random quote from apiQuotes array
+    const quote=localQuotes[Math.floor(Math.random() * localQuotes.length)];
+    // check if author field is blank and replace with 'Unknown'
+    if(quote.authorText == null){
+        authorText.textContent='Unknown';
+    }
+    else{
+        authorText.textContent=quote.author;
+    }
+    // set quote,hide loader
+    quoteText.textContent=quote.text;
+    complete();
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click',newQuote);
+
+newQuote();
